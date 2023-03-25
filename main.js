076de5b9b791a7dd7e57a8cbdbc4f8abcd47febe
Muiz Mabamidu <codegenius012@gmail.com>
2023-03-25 09:59:26 +0100

    Home page Built

---
+++ main.js
@@ -0,0 +1,40 @@
+let name = "Damilola";
+let age = 14;
+let studentShip = true;
+
+
+
+let studentDetails = ["Damilola", 14, true];
+
+
+
+studentDetails.pop();
+studentDetails.pop();
+studentDetails.pop();
+
+
+
+studentDetails.push("Muiz");
+studentDetails.push(2023);
+studentDetails.push(17);
+
+
+studentDetails.shift();
+studentDetails.shift();
+studentDetails.shift();
+
+
+studentDetails.unshift("Muiz");
+studentDetails.unshift(2023);
+studentDetails.unshift(17);
+
+
+
+
+
+
+
+// let studentDetailss = new Array("Damilola",14,true);
+
+
+console.log(studentDetails);
